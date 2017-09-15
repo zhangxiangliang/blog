b483ef972ddc8d19f6133f0ba04a9a60e3cca522
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-15 23:06:25 +0800

    [通用] [服务提供者] 路由服务提供者 添加 Passport 相关路由

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Passport;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -39,6 +40,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        Passport::routes();
         //
     }
 
