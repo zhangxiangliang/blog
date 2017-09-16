2ce022dc0bfc8b995fc3561e76221b24b3881134
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-16 19:00:06 +0800

    [通用] [数据库] 创建 访问记录 迁移文件

---
+++ database/migrations/2017_09_16_101806_create_visitors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned()->comment('访问的文章');
+            $table->string('ip', 32)->comment('IP 地址');
+            $table->string('country')->nullable()->comment('IP 对应的国家');
+            $table->integer('clicks')->unsigned()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitors');
+    }
+}
