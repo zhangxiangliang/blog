aecc26a62caed33404f5cd3727223cd9281d0835
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-19 10:49:51 +0800

    [通用] [数据库] 创建评论表

---
+++ database/migrations/2017_09_18_020205_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->comment('用户 ID');
+            $table->integer('commentable_id')->unsigned()->comment('存放文章或讨论 ID');
+            $table->chat('commentable_type')->comment('区分文章或讨论');
+            $table->text('content')->comment('内容');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
